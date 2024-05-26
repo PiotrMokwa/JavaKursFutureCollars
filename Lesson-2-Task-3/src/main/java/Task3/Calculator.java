631f4e53e19db5a647b72160ee105389e8f958f4
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-05-26 17:15:26 +0200

    Lesson 2 Task 3 version 2

---
+++ Lesson-2-Task-3/src/main/java/Task3/Calculator.java
@@ -0,0 +1,30 @@
+package Task3;
+
+public class Calculator {
+
+
+    public static int sum(int a, int b){
+
+        return a + b;
+    }
+
+    public static int substract(int a, int b){
+
+        return a - b;
+    }
+
+    public static int multiply(int a, int b){
+
+        return a * b;
+    }
+
+    public static float divide(float a, float b){
+
+        return a / b;
+    }
+
+    public static void main(String[] args){
+
+
+    }
+}
---
+++ Lesson-2-Task-3/src/test/java/Task3/CalculatorTest.java
@@ -0,0 +1,66 @@
+package Task3;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void shouldSum() {
+
+        //given a,b
+        int a = 2;
+        int b = 5;
+
+        //when
+        int result = Calculator.sum(a, b);
+
+        //then
+        Assertions.assertEquals(7, result);
+    }
+@Test
+    public void shouldSubstract(){
+
+        //given a,b
+        int a = 2;
+        int b = 5;
+
+        //when
+        int result = Calculator.substract(a,b);
+
+        //then
+        Assertions.assertEquals(-3,result);
+    }
+    @Test
+    public  void shouldMultiply(){
+
+        //given a,b
+        int a = 2;
+        int b = 5;
+
+        //when
+        int result = Calculator.multiply(a,b);
+
+        //then
+        Assertions.assertEquals(10,result);
+    }
+    @Test
+    public void shouldDivide(){
+
+        //given a,b
+        float a = 10;
+        float b = 5;
+
+        //when
+        float result = Calculator.divide(a,b);
+
+        //then
+        Assertions.assertEquals(2,result);
+    }
+
+    public static void main(String[] args){
+
+
+    }
+}
