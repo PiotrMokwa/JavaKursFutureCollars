1275cb28e8090c29e9483ee1e6f4ba536211302d
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-05-29 13:22:07 +0200

    Lesson 2 Task 3 version 3

--- Lesson-2-Task-3/src/test/java/Task3/CalculatorTest.java
+++ Lesson-2-Task-3/src/test/java/Task3/CalculatorTest.java
@@ -1,66 +1,45 @@
 package Task3;
-
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 public class CalculatorTest {
-
     @Test
     public void shouldSum() {
-
         //given a,b
         int a = 2;
         int b = 5;
-
         //when
         int result = Calculator.sum(a, b);
-
         //then
         Assertions.assertEquals(7, result);
     }
-@Test
+@   Test
     public void shouldSubstract(){
-
         //given a,b
         int a = 2;
         int b = 5;
-
         //when
         int result = Calculator.substract(a,b);
-
         //then
         Assertions.assertEquals(-3,result);
     }
     @Test
     public  void shouldMultiply(){
-
         //given a,b
         int a = 2;
         int b = 5;
-
         //when
         int result = Calculator.multiply(a,b);
-
         //then
         Assertions.assertEquals(10,result);
     }
     @Test
-    public void shouldDivide(){
-
+    public void shouldDivide() {
         //given a,b
         float a = 10;
         float b = 5;
-
         //when
-        float result = Calculator.divide(a,b);
-
+        float result = Calculator.divide(a, b);
         //then
-        Assertions.assertEquals(2,result);
-    }
-
-    public static void main(String[] args){
-
-
+        Assertions.assertEquals(2, result);
     }
 }
