d757e5653417939341deb926b110eab0cd33d555
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-05-29 17:30:22 +0200

    Lesson 2 Task 4 version 2

--- Lesson-2-Task-4/src/main/java/Lesson2Task4/Lesson2Task4.java
+++ Lesson-2-Task-4/src/main/java/Lesson2Task4/Lesson2Task4.java
@@ -1,15 +1,6 @@
 package Lesson2Task4;
-
-public class Lesson2Task4 {
-
-public static boolean isAgeGreater18(int age){
-
-    return age > 18;
-}
-
-
-    public static void main(String[] args){
-
-
+    public class Lesson2Task4 {
+    public static boolean isAgeGreater18(int age){
+    return age >= 18;
     }
 }
--- Lesson-2-Task-4/src/test/java/Lesson2Task4/Lesson2Task4Test.java
+++ Lesson-2-Task-4/src/test/java/Lesson2Task4/Lesson2Task4Test.java
@@ -1,14 +1,9 @@
 package Lesson2Task4;
-
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 public class Lesson2Task4Test {
-
 @Test
     public void shouldAgeGreater18(){
-
         //given age
         int age = 20;
         //when
@@ -16,4 +11,23 @@ public class Lesson2Task4Test {
         //then
         Assertions.assertTrue(testAge);
     }
+    @Test
+    public void shouldAgeLower18(){
+        //given age
+        int age = 15;
+        //when
+        boolean testAge =  Lesson2Task4.isAgeGreater18(age);
+        //then
+        Assertions.assertFalse(testAge);
+    }
+    @Test
+    public void shouldAgeEqual18(){
+        //given age
+        int age = 18;
+        //when
+        boolean testAge =  Lesson2Task4.isAgeGreater18(age);
+        //then
+        Assertions.assertTrue(testAge);
+    }
+
 }
