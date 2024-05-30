a8fd2081072f4bdb4c63d53eefc177daf59ef28b
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-05-30 08:36:41 +0200

    Lesson 4 Task 1 version 1

---
+++ Lesson-4-Task-1/src/main/java/Lesson4/Task1/ConsoleMessage.java
@@ -0,0 +1,8 @@
+package Lesson4.Task1;
+import Lesson4.Message;
+public class ConsoleMessage extends Message {
+    @Override
+    public String getMessage() {
+        return "This is class ConsoleMessage";
+    }
+}
---
+++ Lesson-4-Task-1/src/main/java/Lesson4/Task1/Main.java
@@ -0,0 +1,9 @@
+package Lesson4.Task1;
+public class Main {
+    public static void main(String[] args){
+        WindowMessage test1 = new WindowMessage();
+        ConsoleMessage test2 = new ConsoleMessage();
+        System.out.println(test1.getMessage());
+        System.out.println(test2.getMessage());
+    }
+}
