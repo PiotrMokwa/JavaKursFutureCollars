a95563e14a91d299298fd5fc72385529e1787940
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-05-30 21:55:56 +0200

    Lesson 3 Task 5 version 2

--- Lesson-3-Task-5/src/test/java/Lesson3/Task5/MathTest.java
+++ Lesson-3-Task-5/src/test/java/Lesson3/Task5/MathTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class MathTest {
     @Test
-    public  void isTriangleRectangular(){
+    public  void isTriangleRectangularTrue(){
         //given
         int a=4;
         int b=3;
@@ -13,4 +13,16 @@ public class MathTest {
         //then
         Assertions.assertTrue(result);
     }
+    @Test
+    public  void isTriangleRectangularFalse(){
+        //given
+        int a=10;
+        int b=3;
+        int c=5;
+        // when
+        boolean result = Math.isTriangleRectangular(a,b,c);
+        //then
+        Assertions.assertFalse(result);
+    }
+
 }
