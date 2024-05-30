61e131ca88d5fec913dd9105962ee2151231ab7f
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-05-30 08:32:12 +0200

    Lesson 4 Task 1 version 1

---
+++ Lesson-4-Task-1/src/main/java/Lesson4/Task1/WindowMessage.java
@@ -0,0 +1,8 @@
+package Lesson4.Task1;
+import Lesson4.Message;
+public class WindowMessage extends Message {
+    @Override
+    public String getMessage() {
+        return "This is class WindowMessage";
+    }
+}
