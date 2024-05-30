25eeead448ea395d995a1470ade067311428a774
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-05-30 21:23:06 +0200

    Lesson 2 Task 5 version 2

--- Lesson-2-Task-5/src/test/java/Lesson2Task5/Lesson2Task5Test.java
+++ Lesson-2-Task-5/src/test/java/Lesson2Task5/Lesson2Task5Test.java
@@ -1,14 +1,9 @@
 package Lesson2Task5;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 public class Lesson2Task5Test {
-
     @Test
-
-    public void shouldIsNumberOdd(){
-
+    public void shouldIsNumberOddTrue(){
         //given
         int number =3;
         //when
@@ -16,15 +11,31 @@ public class Lesson2Task5Test {
         //then
         Assertions.assertTrue(result);
     }
-
     @Test
-    public void shouldIsNumberEven(){
-
+    public void shouldIsNumberOddFalse(){
+        //given
+        int number =4;
+        //when
+        boolean result = Lesson2Task5.isNumberOdd(number);
+        //then
+        Assertions.assertFalse(result);
+    }
+    @Test
+    public void shouldIsNumberEvenTrue(){
         //given
         int number =4;
         //when
         boolean result = Lesson2Task5.isNumberEven(number);
         //then
         Assertions.assertTrue(result);
+    }
+    @Test
+    public void shouldIsNumberEvenFalse(){
+        //given
+        int number =3;
+        //when
+        boolean result = Lesson2Task5.isNumberEven(number);
+        //then
+        Assertions.assertFalse(result);
     }
 }
