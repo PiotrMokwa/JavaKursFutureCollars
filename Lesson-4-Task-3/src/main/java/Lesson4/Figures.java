edba8c4688c7d5542527df257d569d0140fd0341
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-05-31 14:43:09 +0200

    Lesson 4 Task 3 version 2

---
+++ Lesson-4-Task-3/src/main/java/Lesson4/Figures.java
@@ -0,0 +1,5 @@
+package Lesson4;
+public interface Figures {
+    double getArea(int r);
+    double getPerimeter(int r);
+}
---
+++ Lesson-4-Task-3/src/main/java/Lesson4/Task3/Main.java
@@ -0,0 +1,9 @@
+package Lesson4.Task3;
+public class Main {
+    public static void main(String[] args){
+        Circle circle = new Circle();
+        Square square = new Square();
+        System.out.println(circle.getArea(2));
+        System.out.println(circle.getPerimeter(3));
+    }
+}
