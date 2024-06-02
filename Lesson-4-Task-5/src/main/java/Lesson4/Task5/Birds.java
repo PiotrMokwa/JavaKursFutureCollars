bd5b20d60183d73466320add7064f47769acc2e6
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-06-02 09:56:35 +0200

    Lesson 4 Task 5 version 2

--- Lesson-4-Task-5/src/main/java/Lesson4/Task5/Birds.java
+++ Lesson-4-Task-5/src/main/java/Lesson4/Task5/Birds.java
@@ -1,13 +1,9 @@
 package Lesson4.Task5;
-
 import Lesson4.Animal;
-
 public class Birds extends Animal {
-
     public Birds(String AnimalName) {
         super(AnimalName);
     }
-
     protected String getBirdsName(){
         return super.animalName;
     }
---
+++ Lesson-4-Task-5/src/main/java/Lesson4/Task5/Main.java
@@ -0,0 +1,7 @@
+package Lesson4.Task5;
+public class Main {
+    public static void main(String[] args){
+        Birds parrot = new Birds("Parrot Kakadu");
+        System.out.println(parrot.getBirdsName());
+    }
+}
