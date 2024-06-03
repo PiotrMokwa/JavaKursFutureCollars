e58f945e7f600395dc3d40103a163f2ff2aee3f2
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-06-03 16:26:58 +0200

    Lesson 4 Task 7 version 1

---
+++ Lesson-4-Task-7/src/main/java/Lesson4/Task7/Main.java
@@ -0,0 +1,8 @@
+package Lesson4.Task7;
+import java.util.Arrays;
+public class Main {
+ public enum weekDay{Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+    public static void main(String[] args){
+       Week.dayOfWeek(String.valueOf(weekDay.Friday));
+    }
+}
---
+++ Lesson-4-Task-7/src/main/java/Lesson4/Task7/Week.java
@@ -0,0 +1,6 @@
+package Lesson4.Task7;
+public class Week {
+    public static void dayOfWeek(String day) {
+        System.out.println(day);
+    }
+}
