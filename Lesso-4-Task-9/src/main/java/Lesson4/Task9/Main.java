2d1db001844417fd6a6435fda29d1956a1c390c0
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-06-05 19:53:55 +0200

    Lesson 4 Task 9 version 1

---
+++ Lesso-4-Task-9/src/main/java/Lesson4/Task9/Main.java
@@ -0,0 +1,7 @@
+package Lesson4.Task9;
+public class Main{
+    public static void main(String[] args){
+       Triangle triangle = new Triangle(4.5,10.5);
+       System.out.println(triangle.toString());
+    }
+}
---
+++ Lesso-4-Task-9/src/main/java/Lesson4/Task9/Triangle.java
@@ -0,0 +1,12 @@
+package Lesson4.Task9;
+public class Triangle {
+    double width;
+    double heigt;
+        public Triangle(double width,double heigt){
+            this.width = width;
+            this.heigt = heigt;
+        }
+         public String toString(){
+             return "Triangle{width = " + this.width + ", height = " + this.heigt + "}";
+        }
+}
