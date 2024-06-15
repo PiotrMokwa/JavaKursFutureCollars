babf5fb2ff513b42459067b559ef997bfd7cf8c4
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-06-15 21:16:27 +0200

    Lesson 5 Task 3 version 3

--- Lesson-5-Task-3/src/main/java/Lesson5/Task3/Main.java
+++ Lesson-5-Task-3/src/main/java/Lesson5/Task3/Main.java
@@ -6,6 +6,5 @@ public class Main {
         System.out.println(calculator.getDivide());
         System.out.println(calculator.getSubtract());
         System.out.println(calculator.getMultiply());
-
     }
 }
