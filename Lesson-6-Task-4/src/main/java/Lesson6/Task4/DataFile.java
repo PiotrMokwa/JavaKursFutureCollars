af7eac3725cb539de98b004797e3231c3572b360
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-06-20 10:40:09 +0200

    Lesson 6 Task 4 version 2

--- Lesson-6-Task-4/src/main/java/Lesson6/Task4/DataFile.java
+++ Lesson-6-Task-4/src/main/java/Lesson6/Task4/DataFile.java
@@ -1,24 +1,24 @@
 package Lesson6.Task4;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
 public class DataFile {
-    private final Path path;
-    private String textInFile;
-        public DataFile(String path){
-            this.path = Path.of(path);
+    public DataFile() {
+    }
+    public String read(String path) {
+        String textInFile = "";
+        try {
+            textInFile = Files.readString(Path.of(path));
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
-        public void readFile(){
-            try {
-                this.textInFile = Files.readString(this.path);
-            }catch(IOException exception){
-                exception.printStackTrace();
-            }
-        }
-        public void getWord(CharSequence word){
-            boolean isWordInFile = this.textInFile.contains(word);
-            System.out.println("'" + word + "' text "
-                    + (isWordInFile ? "is in file" : "is not in file: ")
-                    + " " + path);
-        }
+        return textInFile;
+    }
+    public void getWord(CharSequence wordToFind, String textInFile) {
+        boolean isWordInFile = textInFile.contains(wordToFind);
+        System.out.println("'" + wordToFind + "' text "
+                + (isWordInFile ? "is in file" : "is not in file: "));
+    }
 }
--- Lesson-6-Task-4/src/main/java/Lesson6/Task4/Main.java
+++ Lesson-6-Task-4/src/main/java/Lesson6/Task4/Main.java
@@ -1,8 +1,8 @@
 package Lesson6.Task4;
+
 public class Main {
-    public static void  main(String[] args){
-        DataFile file = new DataFile("data.txt");
-        file.readFile();
-        file.getWord("Peter");
+    public static void main(String[] args) {
+        DataFile file = new DataFile();
+        file.getWord("Peter", file.read("data.txt"));
     }
 }
