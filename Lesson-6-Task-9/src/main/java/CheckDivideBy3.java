83e98e58af9fff8a0f980a918d6033885b9da7a2
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-06-20 19:22:35 +0200

    Lesson 6 Task 9 version 2

--- Lesson-6-Task-9/src/main/java/CheckDivideBy3.java
+++ Lesson-6-Task-9/src/main/java/CheckDivideBy3.java
@@ -4,12 +4,12 @@ public class CheckDivideBy3 {
     public void validate(int[] values) {
         boolean isValuesDivideBy3;
         int divideNumber = 3;
-        for (int i = 0; i < values.length; i++) {
-            isValuesDivideBy3 = values[i] % divideNumber == 0;
+        for (int value : values) {
+            isValuesDivideBy3 = value % divideNumber == 0;
             System.out.println(isValuesDivideBy3);
             try {
                 if (!isValuesDivideBy3) {
-                    throw new IllegalArgumentException("Liczba " + values[i] + " nie jest podzielna przez " + divideNumber);
+                    throw new IllegalArgumentException("Liczba " + value + " nie jest podzielna przez " + divideNumber);
                 }
             } catch (IllegalArgumentException info) {
                 System.out.println(info.getMessage());
