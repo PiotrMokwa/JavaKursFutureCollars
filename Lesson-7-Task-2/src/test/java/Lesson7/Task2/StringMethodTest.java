e7960232eec7fa3b8aa9e3693a7ddc41204f4498
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-06-23 08:26:48 +0200

    Lesson 7 Task 2 version 2

--- Lesson-7-Task-2/src/test/java/Lesson7/Task2/StringMethodTest.java
+++ Lesson-7-Task-2/src/test/java/Lesson7/Task2/StringMethodTest.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
 public class StringMethodTest {
     @ParameterizedTest(name = "Transform text '{0}' to upperCase")
     @ValueSource(strings = {"java", "Java"})
