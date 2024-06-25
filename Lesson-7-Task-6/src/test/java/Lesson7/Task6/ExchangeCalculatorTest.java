e53cd86f785ed3f146264ef6311f3170ca8a240d
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-06-25 16:10:58 +0200

    Lesson 7 Task 6 version 2

--- Lesson-7-Task-6/src/test/java/Lesson7/Task6/ExchangeCalculatorTest.java
+++ Lesson-7-Task-6/src/test/java/Lesson7/Task6/ExchangeCalculatorTest.java
@@ -3,76 +3,57 @@ package Lesson7.Task6;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 public class ExchangeCalculatorTest {
-    @ParameterizedTest(name = "Exchange {0} złoty on {1} dollars ")
-    @CsvSource(value = {"10:2.487", "40:9.947"}, delimiter = ':')
-    @DisplayName("Exchange złoty on dollars with saleExchangeRate")
-    public void shouldGetSaleValueInCurrencyDollars(String zlotyCurrencyValue, String dollarCurrencyValue) {
+    private static Stream<Arguments>provideDataForGetSaleValueInCurrency(){
+        return Stream.of(
+                Arguments.of("10","2.487",Currency.$,"4.0212" ),
+                Arguments.of("10","2.289",Currency.€,"4.3679" ),
+                Arguments.of("10","1.959",Currency.£,"5.1046" )
+        );
+    }
+    @ParameterizedTest(name = "Exchange {0} zł on {1} {2} with sale exchange rate {3}")
+    @DisplayName("Exchange złoty with saleExchangeRate")
+    @MethodSource("provideDataForGetSaleValueInCurrency")
+    public void shouldGetSaleValueInCurrencyDollars(String zlotyCurrencyValue, String CurrencyValue, Currency currency, String currencyExchangeRate) {
         //given
         ExchangeCalculator calculate = new ExchangeCalculator();
         BigDecimal zlotyCurrency = new BigDecimal(zlotyCurrencyValue);
-        BigDecimal dollarCurrency = new BigDecimal(dollarCurrencyValue);
+        BigDecimal dollarCurrency = new BigDecimal(CurrencyValue);
+        BigDecimal currencyExchange = new BigDecimal(currencyExchangeRate);
         ExchangeRate mockedExchangeRate = Mockito.mock(ExchangeRate.class);
-        Mockito.when(mockedExchangeRate.getSaleRate(Currency.$)).thenReturn(BigDecimal.valueOf(4.0212));
-
+        Mockito.when(mockedExchangeRate.getSaleRate(currency)).thenReturn(currencyExchange);
         //when
-        BigDecimal result = calculate.getSaleCurrencyValue(zlotyCurrency, Currency.$);
+        BigDecimal result = calculate.getSaleCurrencyValue(zlotyCurrency, currency);
         //then
         Assertions.assertEquals(dollarCurrency, result);
     }
-
-    @ParameterizedTest(name = "Exchange {0} złoty on {1} euro ")
-    @CsvSource(value = {"10:2.289", "40:9.158"}, delimiter = ':')
-    @DisplayName("Exchange złoty on euro with saleExchangeRate")
-    public void shouldGetSaleValueInCurrencyEuro(String zlotyCurrencyValue, String euroCurrencyValue) {
-        //given
-        ExchangeCalculator calculate = new ExchangeCalculator();
-        BigDecimal zlotyCurrency = new BigDecimal(zlotyCurrencyValue);
-        BigDecimal dollarCurrency = new BigDecimal(euroCurrencyValue);
-        ExchangeRate mockedExchangeRate = Mockito.mock(ExchangeRate.class);
-        Mockito.when(mockedExchangeRate.getSaleRate(Currency.€)).thenReturn(BigDecimal.valueOf(4.3679));
-
-        //when
-        BigDecimal result = calculate.getSaleCurrencyValue(zlotyCurrency, Currency.€);
-        //then
-        Assertions.assertEquals(dollarCurrency, result);
+    private static Stream<Arguments>provideDataForGetPurchaseValueInCurrencyDollars(){
+        return Stream.of(
+                Arguments.of("10","2.516",Currency.$,"3.9739" ),
+                Arguments.of("10","2.334",Currency.€,"4.2838" ),
+                Arguments.of("10","1.963",Currency.£,"5.0949" )
+        );
     }
-
-    @ParameterizedTest(name = "Exchange {0} złoty on {1} dollars")
-    @CsvSource(value = {"10:2.516", "40:10.066"}, delimiter = ':')
-    @DisplayName("Exchange złoty on dollars with purchaseExchangeRate")
-    public void shouldGetPurchaseValueInCurrencyDollars(String zlotyCurrencyValue, String dollarCurrencyValue) {
+    @ParameterizedTest(name = "Exchange {0} zł on {1} {2} with purchase exchange rate {3}")
+    @MethodSource("provideDataForGetPurchaseValueInCurrencyDollars")
+    @DisplayName("Exchange złoty with purchaseExchangeRate")
+    public void shouldGetPurchaseValueInCurrencyDollars(String zlotyCurrencyValue, String CurrencyValue, Currency currency, String currencyExchangeRate) {
         //given
         ExchangeCalculator calculate = new ExchangeCalculator();
         BigDecimal zlotyCurrency = new BigDecimal(zlotyCurrencyValue);
-        BigDecimal dollarCurrency = new BigDecimal(dollarCurrencyValue);
+        BigDecimal dollarCurrency = new BigDecimal(CurrencyValue);
+        BigDecimal currencyExchange = new BigDecimal(currencyExchangeRate);
         ExchangeRate mockedExchangeRate = Mockito.mock(ExchangeRate.class);
-        Mockito.when(mockedExchangeRate.getPurchaseRate(Currency.$)).thenReturn(BigDecimal.valueOf(3.9739));
-
+        Mockito.when(mockedExchangeRate.getPurchaseRate(currency)).thenReturn(currencyExchange);
         //when
-        BigDecimal result = calculate.getPurchaseCurrencyValue(zlotyCurrency, Currency.$);
-        //then
-        Assertions.assertEquals(dollarCurrency, result);
-    }
-
-    @ParameterizedTest(name = "Exchange {0} złoty on {1} euro ")
-    @CsvSource(value = {"10:2.334", "40:9.338"}, delimiter = ':')
-    @DisplayName("Exchange złoty on euro with purchaseExchangeRate")
-    public void shouldGetPurchaseValueInCurrencyEuro(String zlotyCurrencyValue, String dollarCurrencyValue) {
-        //given
-        ExchangeCalculator calculate = new ExchangeCalculator();
-        BigDecimal zlotyCurrency = new BigDecimal(zlotyCurrencyValue);
-        BigDecimal dollarCurrency = new BigDecimal(dollarCurrencyValue);
-        ExchangeRate mockedExchangeRate = Mockito.mock(ExchangeRate.class);
-        Mockito.when(mockedExchangeRate.getPurchaseRate(Currency.€)).thenReturn(BigDecimal.valueOf(4.2838));
-
-        //when
-        BigDecimal result = calculate.getPurchaseCurrencyValue(zlotyCurrency, Currency.€);
+        BigDecimal result = calculate.getPurchaseCurrencyValue(zlotyCurrency, currency);
         //then
         Assertions.assertEquals(dollarCurrency, result);
     }
