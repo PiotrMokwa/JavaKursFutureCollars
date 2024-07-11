e72a7fd8a36719e5904088e6af8f1e718782db9d
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-07-11 18:45:49 +0200

    Lesson 9 Task 4 version 2

--- Lesson-9-Task-4/src/main/java/Lesson9/Task4/Main.java
+++ Lesson-9-Task-4/src/main/java/Lesson9/Task4/Main.java
@@ -2,10 +2,10 @@ package Lesson9.Task4;
 public class Main {
     public static void main(String[] args){
         Pair<String> pair = new Pair<>("Peter","Tomm");
-       System.out.println(pair.getValueK());
-       System.out.println(pair.getValueV());
-       Pair<Integer> secondPair = new Pair<>(1,2);
-       System.out.println(secondPair.getValueK());
-       System.out.println(secondPair.getValueV());
+           System.out.println(pair.getValueK());
+           System.out.println(pair.getValueV());
+           Pair<Integer> secondPair = new Pair<>(1,2);
+           System.out.println(secondPair.getValueK());
+           System.out.println(secondPair.getValueV());
     }
 }
