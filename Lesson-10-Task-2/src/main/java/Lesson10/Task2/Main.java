e64564851e70f344c0965e81769dc156ec7f3990
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-07-15 05:26:56 +0200

    Lesson 10 Task 2 version 2

--- Lesson-10-Task-2/src/main/java/Lesson10/Task2/Main.java
+++ Lesson-10-Task-2/src/main/java/Lesson10/Task2/Main.java
@@ -1,16 +1,12 @@
 package Lesson10.Task2;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Main {
     public static void main(String[] args){
         String  numberPESEL = "02032034562";
-        NumberPeselValidate.isNumberPeselCorect(numberPESEL);
+        NumberPESELValidate.isNumberPESELCorrect(numberPESEL);
         numberPESEL = "0#032034562";
-        NumberPeselValidate.isNumberPeselCorect(numberPESEL);
+        NumberPESELValidate.isNumberPESELCorrect(numberPESEL);
         numberPESEL = "0#03";
-        NumberPeselValidate.isNumberPeselCorect(numberPESEL);
+        NumberPESELValidate.isNumberPESELCorrect(numberPESEL);
     }
 
 }
--- Lesson-10-Task-2/src/main/java/Lesson10/Task2/NumberPESELValidate.java
+++ Lesson-10-Task-2/src/main/java/Lesson10/Task2/NumberPESELValidate.java
@@ -1,11 +1,8 @@
 package Lesson10.Task2;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-public abstract class NumberPeselValidate {
-    public static  void isNumberPeselCorect(String numberPESEL){
-
+public abstract class NumberPESELValidate {
+    public static  void isNumberPESELCorrect(String numberPESEL){
         Pattern pattern = Pattern.compile("\\d{11}");
         Matcher matcher = pattern.matcher(numberPESEL);
         boolean matches = matcher.matches();
