49b858903b94b820f21ff01beb28703077cd36d1
Piotr.Mokwa <mokwapiotr@gmail.com>
2024-07-18 06:35:28 +0200

    Lesson 11 Task 2 version 1

---
+++ Lesson-11-Task-2/src/main/java/Lesson11/Ingredients.java
@@ -0,0 +1,9 @@
+package Lesson11;
+public enum Ingredients {     mozarella
+                            , champignons
+                            , salami
+                            , tomatto
+                            , pepper
+                            , celery
+                            , onion
+                            }
